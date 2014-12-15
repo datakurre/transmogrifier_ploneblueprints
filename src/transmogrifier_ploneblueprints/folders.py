494fa88e5ef5747d00dfca2409d5579ea364029e
Erkka Heinilä <erkka.heinila@jyu.fi>
2014-12-15 17:08:57 +0200

    Make folders section conditional

--- src/transmogrifier_ploneblueprints/folders.py
+++ src/transmogrifier_ploneblueprints/folders.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from venusianconfiguration import configure
-from transmogrifier.blueprints import Blueprint
+from transmogrifier.blueprints import ConditionalBlueprint
 
 from transmogrifier.utils import defaultMatcher
 
@@ -13,7 +13,7 @@ from transmogrifier_ploneblueprints.utils import traverse
 
 # noinspection PyPep8Naming
 @configure.transmogrifier.blueprint.component(name='plone.folders')
-class FoldersSection(Blueprint):
+class FoldersSection(ConditionalBlueprint):
     def __iter__(self):
         pathKeyMatcher = defaultMatcher(self.options, 'path-key', self.name, 'path')
 
@@ -26,49 +26,48 @@ class FoldersSection(Blueprint):
         seen = set()
 
         for item in self.previous:
+            if self.condition(item):
+                keys = item.keys()
+                pathKey = pathKeyMatcher(*keys)[0]
 
-            keys = item.keys()
-            pathKey = pathKeyMatcher(*keys)[0]
+                if not pathKey:  # not enough info
+                    yield item
+                    continue
 
-            if not pathKey:  # not enough info
-                yield item
-                continue
+                newPathKey = newPathKey or pathKey
+                newTypeKey = newTypeKey
 
-            newPathKey = newPathKey or pathKey
-            newTypeKey = newTypeKey
+                path = item[pathKey]
+                elements = path.strip('/').rsplit('/', 1)
+                container, id_ = (len(elements) == 1
+                                  and ('', elements[0]) or elements)
 
-            path = item[pathKey]
-            elements = path.strip('/').rsplit('/', 1)
-            container, id_ = (len(elements) == 1
-                              and ('', elements[0]) or elements)
+                # This may be a new container
+                if container not in seen:
 
-            # This may be a new container
-            if container not in seen:
+                    container_path_items = list(pathsplit(container))
+                    if container_path_items:
 
-                container_path_items = list(pathsplit(container))
-                if container_path_items:
+                        currentElement = []
 
-                    currentElement = []
+                        # Check each possible parent folder
+                        obj = self.transmogrifier.context
+                        for element in container_path_items:
+                            currentElement.append(element)
+                            currentPath = '/'.join(currentElement)
 
-                    # Check each possible parent folder
-                    obj = self.transmogrifier.context
-                    for element in container_path_items:
-                        currentElement.append(element)
-                        currentPath = '/'.join(currentElement)
+                            if currentPath and currentPath not in seen:
 
-                        if currentPath and currentPath not in seen:
+                                if element and traverse(obj, element) is None:
+                                    # We don't have this path - yield to create
+                                    # a skeleton folder
+                                    yield {newPathKey: '/' + currentPath,
+                                           newTypeKey: folderType}
+                                if cache:
+                                    seen.add(currentPath)
 
-                            if element and traverse(obj, element) is None:
-                                # We don't have this path - yield to create a
-                                # skeleton folder
-                                yield {newPathKey: '/' + currentPath,
-                                       newTypeKey: folderType}
-                            if cache:
-                                seen.add(currentPath)
+                            obj = traverse(obj, element)
 
-                        obj = traverse(obj, element)
-
-            if cache:
-                seen.add('{0:s}/{1:s}'.format(container, id_, ))
-
+                if cache:
+                    seen.add('{0:s}/{1:s}'.format(container, id_, ))
             yield item
