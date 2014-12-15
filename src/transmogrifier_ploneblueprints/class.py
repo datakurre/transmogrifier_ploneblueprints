79098b26caf14ee68e76e9f6935aea9dc365dcf5
Erkka Heinilä <erkka.heinila@jyu.fi>
2014-12-15 17:05:12 +0200

    Dig correct class information in change_class section

--- src/transmogrifier_ploneblueprints/class.py
+++ src/transmogrifier_ploneblueprints/class.py
@@ -1,6 +1,5 @@
 import importlib
-from io import BytesIO
-from pprint import pprint
+from Products.Archetypes.ArchetypeTool import _types
 from Products.CMFCore.utils import getToolByName
 from plone.dexterity.interfaces import IDexterityFTI
 from venusianconfiguration import configure
@@ -15,24 +14,24 @@ logger = logging.getLogger('transmogrifier')
 class ChangeClassSection(ConditionalBlueprint):
 
     def _change_class(self, ob, item):
-        get_type = getattr(ob, 'getPortalTypeName', lambda x: '')
+        get_type = getattr(ob, 'getPortalTypeName', lambda x: None)
         if get_type() not in self.options.get('types', []):
             return
         portal_types = getToolByName(self.transmogrifier.context,
                                      'portal_types')
         fti = portal_types.get(item['_type'])
         is_dexterity = IDexterityFTI.providedBy(fti)
-        # TODO: Not implemented yet
-        if is_dexterity:
-            cls = None
-        else:
-            cls = None
 
-        if cls:
-            module_name, class_name = cls.rsplit('.', 1)
+        if is_dexterity:
+            module_name, class_name = fti.klass.rsplit('.', 1)
             module = importlib.import_module(module_name)
-            ob.__class__ = getattr(module, class_name)
-            ob._p_changed = True
+            class_ = getattr(module, class_name)
+        else:
+            key = ".".join([fti.product, fti.id])
+            class_ = _types[key].klass
+
+        ob.__class__ = class_
+        ob._p_changed = True
 
     def __iter__(self):
         for item in self.previous:
