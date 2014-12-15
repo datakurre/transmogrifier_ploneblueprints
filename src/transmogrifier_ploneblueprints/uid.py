afe342bcdbf6ef339f52cbdd919ae7d4e0c9b6ca
Matti Pohjanvirta <matti.l.pohjanvirta@student.jyu.fi>
2014-12-15 12:55:25 +0200

    Make get and set UUID blueprints

--- src/transmogrifier_ploneblueprints/uid.py
+++ src/transmogrifier_ploneblueprints/uid.py
@@ -1,5 +1,6 @@
 from venusianconfiguration import configure
 from transmogrifier.blueprints import Blueprint
+from transmogrifier.blueprints import ConditionalBlueprint
 from Products.Archetypes.interfaces import IReferenceable as \
     ATIReferenceable
 from Products.Archetypes.config import UUID_ATTR as AT_UID_ATTR
@@ -7,12 +8,24 @@ from plone.app.referenceablebehavior.referenceable import IReferenceable as \
     DXIReferenceable
 from plone.uuid.interfaces import ATTRIBUTE_NAME as DX_UID_ATTR, IUUID
 
+
+@configure.transmogrifier.blueprint.component(name='plone.get_uuid')
+class GetUUID(ConditionalBlueprint):
+    def __iter__(self):
+        key = self.options.get('key')
+        for item in self.previous:
+            if self.condition(item):
+                ob = item.get(key)
+                if ob is not None and IUUID.providedBy(ob):
+                    item['_uid'] = IUUID(ob)
+            yield item
+
+
 # sc/transmogrifier/sections/universal_uid_updater.py
 #by jsbueno
 
-
-@configure.transmogrifier.blueprint.component(name='plone.uid')
-class UniversalUIDUpdater(Blueprint):
+@configure.transmogrifier.blueprint.component(name='plone.set_uuid')
+class SetUUID(ConditionalBlueprint):
     """Sets UID for both AT and DX content types
     The UID blueprint in collective.transmogrifier
     can't deal with dexterity content.
@@ -22,23 +35,26 @@ class UniversalUIDUpdater(Blueprint):
     """
     def __iter__(self):
         for item in self.previous:
+            if self.condition(item):
+                self._set_uuid(item)
+            yield item
 
+    def _set_uuid(self, item):
             portal = self.transmogrifier.context
             path = "".join(portal.getPhysicalPath()) + item['_path']
-
-            if isinstance(path, unicode):
-                path = path.encode('UTF-8')
 
             obj = portal.unrestrictedTraverse(path)
             uid = item.get('_uid')
 
-            if not uid:
-                pass
+            # if not uid:
+            #     import pdb; pdb.set_trace()
 
             at_uid = ATIReferenceable.providedBy(obj)
             dx_uid = DXIReferenceable.providedBy(obj)
-
-            old_uid = IUUID(obj)
+            try:
+                old_uid = IUUID(obj)
+            except TypeError:
+                import pdb; pdb.set_trace()
             if old_uid != uid:
                 # Code from plone.app.transmogrifier used for AT objects:
                 if at_uid:
@@ -54,8 +70,6 @@ class UniversalUIDUpdater(Blueprint):
                 #     # and at least the desired UID value stays recorded in the
                 #     # object, allowing for a post-migration retrieval
                 #     setattr(obj, DEFAULT_UID_ATTR, uid)
-
-            yield item
 
     # OPTIONS = [("uidkey", "_uid", "string")]
 
