b41bc4ced8d157fd10511d5e7455caac4f91e568
Asko Soukka <asko.soukka@iki.fi>
2014-12-29 11:34:48 +0200

    Add plone.reindex_object -blueprint

---
+++ src/transmogrifier_ploneblueprints/catalog.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from transmogrifier.blueprints import ConditionalBlueprint
+from transmogrifier_ploneblueprints.utils import traverse
+from venusianconfiguration import configure
+
+
+@configure.transmogrifier.blueprint.component(name='plone.reindex_object')
+class ReindexObject(ConditionalBlueprint):
+    def __iter__(self):
+        portal = api.portal.get()
+        for item in self.previous:
+            if self.condition(item):
+                ob = traverse(portal, item['_path'])
+                ob.reindexObject()
+            yield item
