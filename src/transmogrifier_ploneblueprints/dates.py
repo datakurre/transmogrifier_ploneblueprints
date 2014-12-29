dc5d4d069f3c0ae43baaaff1f8700b04988c6d73
Erkka Heinilä <erkka.heinila@jyu.fi>
2014-12-29 16:51:11 +0200

    Add date setters to import pipeline

---
+++ src/transmogrifier_ploneblueprints/dates.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from transmogrifier.blueprints import ConditionalBlueprint
+from transmogrifier_ploneblueprints.utils import traverse
+from venusianconfiguration import configure
+
+@configure.transmogrifier.blueprint.component(name='plone.fix_dates')
+class FixDates(ConditionalBlueprint):
+    def __iter__(self):
+        portal = api.portal.get()
+        for item in self.previous:
+            if self.condition(item):
+                ob = traverse(portal, item['_path'])
+                ob.setModificationDate(item['modification_date'])
+                try:
+                    ob.setCreationDate(item['creation_date'])
+                except AttributeError:
+                    # dexterity content does not have setCreationDate
+                    ob.creation_date = item['creation_date']
+            yield item
