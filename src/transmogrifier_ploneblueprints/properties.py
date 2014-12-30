d18266cee2f247e84a95455d3d819862b2cbc1c7
Erkka Heinilä <erkka.heinila@jyu.fi>
2014-12-30 11:32:58 +0200

    Add import structure pipeline and add property setter blueprint

--- src/transmogrifier_ploneblueprints/properties.py
+++ src/transmogrifier_ploneblueprints/properties.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from transmogrifier.blueprints import ConditionalBlueprint
+from transmogrifier_ploneblueprints.utils import traverse
 from venusianconfiguration import configure
 
 
@@ -9,6 +11,17 @@ class GetProperties(ConditionalBlueprint):
         for item in self.previous:
             if self.condition(item):
                 ob = item['_object']
-                properties = [(key, (ob.getProperty(key), ob.getPropertyType(key))) for key in ob.propertyIds()] 
-                item.update(dict(properties))
+                properties = [(key, ob.getProperty(key), ob.getPropertyType(key)) 
+                              for key in ob.propertyIds()] 
+                item['properties'] = properties
+            yield item
+
+@configure.transmogrifier.blueprint.component(name='plone.properties.set')
+class SetProperties(ConditionalBlueprint):
+    def __iter__(self):
+        portal = api.portal.get()
+        for item in self.previous:
+            if self.condition(item):
+                ob = traverse(portal, item['_path'])
+                # set properties
             yield item
