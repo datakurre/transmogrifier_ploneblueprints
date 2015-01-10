78175a1d5ffea4066bdf1f22f90b92bf27864058
Asko Soukka <asko.soukka@iki.fi>
2015-01-10 03:12:32 +0200

    Fix placeful workflow blueprint to skip itself when tool is not available

--- src/transmogrifier_ploneblueprints/placeful_workflow.py
+++ src/transmogrifier_ploneblueprints/placeful_workflow.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
-from Products.CMFCore.utils import getToolByName
-from transmogrifier.blueprints import ConditionalBlueprint
-from transmogrifier_ploneblueprints.utils import traverse
+from plone.api.exc import InvalidParameterError
+
 from venusianconfiguration import configure
 from plone import api
 from Acquisition import aq_base
 
+from transmogrifier.blueprints import ConditionalBlueprint
+from transmogrifier_ploneblueprints.utils import traverse
+
+
 @configure.transmogrifier.blueprint.component(name='plone.placeful_workflow.get')  # noqa
 class GetPlacefulWorkflow(ConditionalBlueprint):
     def __iter__(self):
-        portal = api.portal.get()
-        pwtool = getToolByName(portal, "portal_placeful_workflow")
+        try:
+            pwtool = api.portal.get_tool("portal_placeful_workflow")
+        except InvalidParameterError:
+            pwtool = None
         for item in self.previous:
-            if self.condition(item):
+            if pwtool and self.condition(item):
                 ob = item['_object']
                 config = pwtool.getWorkflowPolicyConfig(ob)
                 if config is not None and (config.getPolicyInId()
@@ -23,36 +28,40 @@ class GetPlacefulWorkflow(ConditionalBlueprint):
 
 
 def updateRoleMappings(container):
-    wftool = getToolByName(container, "portal_workflow")
+    wftool = api.portal.get_tool("portal_workflow")
     wfs = {}
     for id_ in wftool.objectIds():
         wf = wftool.getWorkflowById(id_)
         if hasattr(aq_base(wf), "updateRoleMappingsFor"):
             wfs[id_] = wf
     count = wftool._recursiveUpdateRoleMappings(aq_base(container), wfs)
-    return count 
+    return count
 
 
 @configure.transmogrifier.blueprint.component(name='plone.placeful_workflow.set')  # noqa
 class SetPlacefulWorkflow(ConditionalBlueprint):
     def __iter__(self):
         portal = api.portal.get()
-        pwtool = getToolByName(portal, "portal_placeful_workflow")
+        try:
+            pwtool = api.portal.get_tool("portal_placeful_workflow")
+        except InvalidParameterError:
+            pwtool = None
+
         for item in self.previous:
-            if self.condition(item) and '_workflow_policy_in' in item:
+            if (pwtool and self.condition(item)
+                    and '_workflow_policy_in' in item):
                 ob = traverse(portal, item['_path'])
 
                 # Init placeful workflow policy config when required
                 if pwtool.getWorkflowPolicyConfig(ob) is None:
                     local_tool = ob.manage_addProduct["CMFPlacefulWorkflow"]
-                    local_tool.manage_addWorkflowPolicyConfig() 
-                
+                    local_tool.manage_addWorkflowPolicyConfig()
+
                 # Set the policy
                 config = pwtool.getWorkflowPolicyConfig(ob)
                 config.setPolicyIn(item.get('_workflow_policy_in', ''))
                 config.setPolicyBelow(item.get('_workflow_policy_below', ''))
 
                 # Update security
-                updateRoleMappings(ob) 
+                updateRoleMappings(ob)
             yield item
-
